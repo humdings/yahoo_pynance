3a9d3e358adc01065e095c7896e5110e99380faf
David Edwards <dedwards@rohan.sdsu.edu>
2014-01-13 16:26:59 -0800

    BUG: fixed bad commit

--- yahoo_pynance/api.py
+++ yahoo_pynance/api.py
@@ -60,12 +60,6 @@ sector_dict:
      ...
     }
     
-CLASSES:
-    StockHistory
-    StockChart
-    Stock
-
-see classes for more
 
 '''
 
@@ -230,7 +224,7 @@ def sector_dict(**kwargs):
     cols = [i.pop(0) for i in cr]
     d = {
         cols[i]: {
-            idx[j]: cr[i][j]for j in range(len(idx))
+            idx[j]: cr[i][j] for j in range(len(idx))
         } for i in range(len(cols))
     }
     for i in d:
@@ -311,32 +305,41 @@ class Stock(object):
         self.update() ==> call self.__init__() to update quotes.
     
     '''
-    
+    fields = {
+        'price': 'l1', 
+        'change':'c1', 
+        'volume':'v',
+        'avg_daily_volume': 'a2',
+        'stock_exchange': 'x',
+        'market_cap':'j1',
+        'book_value': 'b4',
+        'ebitda': 'j4', 
+        'dividend_per_share': 'd',
+        'dividend_yield': 'y',
+        'earnings_per_share': 'e',
+        'fifty_two_week_high': 'k',
+        'fifty_two_week_low': 'j', 
+        'fifty_day_moving_avg': 'm3',
+        'two_hundred_day_moving_avg':'m4',
+        'price_earnings_ratio':'r',
+        'price_earnings_growth_ratio':'r5',
+        'price_sales_ratio':'p5',
+        'price_book_ratio': 'p6',
+        'short_ratio': 's7', 
+        'revenue': 's6',
+        'shares_outstanding': 'j2',
+        'shares_owned': 's1',
+        'pct_change_from_50_day_MA':'m8',
+        'pct_change_from_200_day_MA': 'm6', 
+        'one_yr_target_price': 't8',
+        'dividend_pay_date': 'r1',
+    }
     def __init__(self, symbol):
         self.symbol = symbol
         self.all = self._all_quote_data()
-        self.timestamp = datetime.datetime.now()                
-        self.all['timestamp'] = self.timestamp
-        self.price = self.all['price']
-        self.change = self.all['change']
-        self.volume = self.all['volume']
-        self.avg_daily_volume = self.all['avg_daily_volume']
-        self.stock_exchange = self.all['stock_exchange']
-        self.market_cap = self.all['market_cap']
-        self.book_value = self.all['book_value']
-        self.ebitda = self.all['ebitda']
-        self.dividend_per_share = self.all['dividend_per_share']
-        self.dividend_yield = self.all['dividend_yield']
-        self.earnings_per_share = self.all['earnings_per_share']
-        self.fifty_two_week_high = self.all['fifty_two_week_high']
-        self.fifty_two_week_low = self.all['fifty_two_week_low']
-        self.fifty_day_moving_avg = self.all['fifty_day_moving_avg']
-        self.two_hundred_day_moving_avg = self.all['two_hundred_day_moving_avg']
-        self.price_earnings_ratio = self.all['price_earnings_ratio']
-        self.price_earnings_growth_ratio = self.all['price_earnings_growth_ratio']
-        self.price_sales_ratio = self.all['price_sales_ratio']
-        self.price_book_ratio = self.all['price_book_ratio']
-        self.short_ratio = self.all['short_ratio']
+        self.timestamp = datetime.datetime.now()
+        for quote in self.all:
+            self.__dict__[quote] = self.all[quote]
     
     def __getitem__(self,item):
         return self.all[item]
@@ -357,29 +360,7 @@ class Stock(object):
         return str(response.read().decode('utf-8').strip())
     
     def _all_quote_data(self):
-        values = self._quote_request('l1c1va2xj1b4j4dyekjm3m4rr5p5p6s7').split(',')
-        quotes = dict(
-            price = values[0],
-            change = values[1],
-            volume = values[2],
-            avg_daily_volume = values[3],
-            stock_exchange = values[4],
-            market_cap = values[5],
-            book_value = values[6],
-            ebitda = values[7],
-            dividend_per_share = values[8],
-            dividend_yield = values[9],
-            earnings_per_share = values[10],
-            fifty_two_week_high = values[11],
-            fifty_two_week_low = values[12],
-            fifty_day_moving_avg = values[13],
-            two_hundred_day_moving_avg = values[14],
-            price_earnings_ratio = values[15],
-            price_earnings_growth_ratio = values[16],
-            price_sales_ratio = values[17],
-            price_book_ratio = values[18],
-            short_ratio = values[19],
-        )        
+        quotes = {i: self._quote_request(self.fields[i]) for i in self.fields}
         for q in quotes:
             try:
                 quotes[q] = float(quotes[q])
@@ -391,9 +372,7 @@ class Stock(object):
         return StockChart(self.symbol, **kwargs)
     
     def history(self, start_date, end_date, **kwargs):
-        return StockHistory(
-            self.symbol, start_date, end_date, **kwargs
-        )
+        return StockHistory(self.symbol, start_date, end_date, **kwargs)
+        
     def update(self):
         self.__init__(self.symbol)
-
