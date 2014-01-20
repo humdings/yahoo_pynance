887e900dfb4c888e7d8818e3a309533b38d7e99f
David Edwards <dedwards@rohan.sdsu.edu>
2014-01-19 20:32:09 -0800

    Deleted Data Nitro scripts

--- yahoo_pynance/data_nitro_scripts/portfolio.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-# Welcome to the DataNitro Editor
-# Use Cell(row,column).value, or Cell(name).value, to read or write to cells
-# Cell(1,1) and Cell("A1") refer to the top-left cell in the spreadsheet
-# 
-# Note: To run this file, save it and run it from Excel (click "Run from File")
-# If you have trouble saving, try using a different directory
-
-
